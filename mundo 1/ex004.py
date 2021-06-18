e3afb93ed71e770cd64daa2a71c2eaa54c8bfa9f
Ramon <ramonerik29a@gmail.com>
2021-06-18 15:43:11 -0300

    Update ex004.py
    adicionei mais um .is() ao ex 004

--- mundo 1/ex004.py
+++ mundo 1/ex004.py
@@ -2,4 +2,5 @@ v1 = input('Digit qualquer coisa: ')
 print('Is all num?', v1.isalnum())
 print(f'Is all alpha? {v1.isalpha()}')
 print(f'Is space? {v1.isspace()}')
+print(f'Is upper? {v1.isupper()} ')
 #há outros
