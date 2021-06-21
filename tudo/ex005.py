2ba3b90ed423a7e34e562cb346ce221c8cc0d20d
Ramon <ramonerik29a@gmail.com>
2021-06-21 01:49:32 -0300

    Update ex005.py
    Tentei deixar o ex005 mais legível

--- tudo/ex005.py
+++ tudo/ex005.py
@@ -1,4 +1,4 @@
-n = int(input('Digite um número: '))
-a = 1-n
-s = 1+n
-print('O antecessor de {} é {} /n e o sucessor é {}'.format(n, a, s))
+num = int(input('Digite um número: '))
+ant = 1 - num # antecessor
+suc = 1 + num # sucessor
+print('O antecessor de {} é {} /n e o sucessor é {}'.format(num, ant, suc))
